2dd4deb4c17147e171e14e38632d4574d0031a5e
NobreTonto <137806075+simpsonGO@users.noreply.github.com>
2025-05-23 12:44:01 +0000

    New Pages

--- Script/script_main.js
+++ Script/script_main.js
@@ -1,7 +1,5 @@
-  <script>
-    const scrollTopBtn = document.getElementById('scrollTop');
-    window.onscroll = function () {
-      scrollTopBtn.style.display = window.scrollY > 300 ? 'block' : 'none';
-    };
-    scrollTopBtn.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' });
-  </script>
+const scrollTopBtn = document.getElementById('scrollTop');
+window.onscroll = function () {
+    scrollTopBtn.style.display = window.scrollY > 300 ? 'block' : 'none';
+};
+scrollTopBtn.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' });
